3e64acc2d4ce4f9f741c476f21d9b9361e723c3a
Vukasin Radovanovic <vukasinradovanovic@gmail.com>
2026-05-14 02:05:58 +0200

    Added login and register pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Login } from './auth/login/login';
+import { Register } from './auth/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Login
+    },
+    {
+        path: 'register',
+        component: Register
+    },
+];
