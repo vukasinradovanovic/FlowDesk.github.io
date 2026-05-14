1962888c1dc42f29165e79da1d56bf8823a5c0a6
Vukasin Radovanovic <vukasinradovanovic@gmail.com>
2026-05-14 20:30:48 +0200

    Init for dashboard

---
+++ src/app/dashboard/index/index.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-index',
+	imports: [],
+	templateUrl: './index.html',
+	styleUrl: './index.scss',
+})
+export class Index {}
