1ca2d4ff6f56b4722612d3917276c2fd6ed22d92
Vukasin Radovanovic <vukasinradovanovic@gmail.com>
2026-05-22 13:30:05 +0200

    Hot fix for projects

--- src/app/layout/nav/main-nav-dashboard/main-nav-dashboard.ts
+++ src/app/layout/nav/main-nav-dashboard/main-nav-dashboard.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from "@angular/router";
+import { AuthService } from '../../../services/auth/auth.service';
+import { ProjectService } from '../../../services/project/project';
 
 interface NavLink {
   label: string;
@@ -15,16 +17,31 @@ interface NavLink {
 	templateUrl: './main-nav-dashboard.html',
 	styleUrl: './main-nav-dashboard.scss',
 })
-export class MainNavDashboard {
-	navLinks: NavLink[] = [
-    { label: 'Home', icon: 'bi bi-grid-1x2', route: ['/dashboard'], exact: true },
-    { label: 'Projects', icon: 'bi bi-folder2-open', route: ['/dashboard', 'projects'], adons: '12' },
-	// { label: 'Team', icon: 'bi bi-people', route: ['/dashboard', 'team'] },
-    // { label: 'Kanban Board', icon: 'bi bi-kanban', route: ['/dashboard', 'settings'] },
-	// { label: 'Calendar', icon: 'bi bi-calendar3', route: ['/dashboard', 'calendar'] },
-	// { label: 'Analytics', icon: 'bi bi-bar-chart', route: ['/dashboard', 'analytics'] },
-	// { label: 'Notifications', icon: 'bi bi-bell', route: ['/dashboard', 'team'], adons: '5' },
-	// { label: 'Workspace', icon: 'bi bi-gear', route: ['/dashboard', 'team'] },
-	// { label: 'Settings', icon: 'bi bi-question-circle', route: ['/dashboard', 'team'] },
-  ];
+export class MainNavDashboard implements OnInit {
+	auth = inject(AuthService);
+	project = inject(ProjectService);
+	
+	projectsCount: number = 0;
+
+	get navLinks(): NavLink[] {
+		return [
+			{ label: 'Home', icon: 'bi bi-grid-1x2', route: ['/dashboard'], exact: true },
+			{ label: 'Projects', icon: 'bi bi-folder2-open', route: ['/dashboard', 'projects'], adons: this.projectsCount.toString() },
+			// { label: 'Team', icon: 'bi bi-people', route: ['/dashboard', 'team'] },
+			// { label: 'Kanban Board', icon: 'bi bi-kanban', route: ['/dashboard', 'settings'] },
+			// { label: 'Calendar', icon: 'bi bi-calendar3', route: ['/dashboard', 'calendar'] },
+			// { label: 'Analytics', icon: 'bi bi-bar-chart', route: ['/dashboard', 'analytics'] },
+			// { label: 'Notifications', icon: 'bi bi-bell', route: ['/dashboard', 'team'], adons: '5' },
+			// { label: 'Workspace', icon: 'bi bi-gear', route: ['/dashboard', 'team'] },
+			// { label: 'Settings', icon: 'bi bi-question-circle', route: ['/dashboard', 'team'] },
+		];
+	}
+
+	ngOnInit() {
+		if (this.auth.currentUser?.id) {
+			this.project.getProjects().subscribe(projects => {
+				this.projectsCount = projects.length;
+			});
+		}
+	}
 }
