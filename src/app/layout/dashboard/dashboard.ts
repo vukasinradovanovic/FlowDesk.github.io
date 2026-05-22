be43bea706738ae46571005ae1ddb0e69afd42a3
Vukasin Radovanovic <vukasinradovanovic@gmail.com>
2026-05-22 03:22:32 +0200

    Footer separated

--- src/app/layout/dashboard/dashboard.ts
+++ src/app/layout/dashboard/dashboard.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Sidebar } from '../sidebar/sidebar';
 import { Header } from '../header/header';
+import { Footer } from "../footer/footer/footer";
 
 @Component({
 	selector: 'app-dashboard',
-	imports: [RouterOutlet, Sidebar, Header],
+	imports: [RouterOutlet, Sidebar, Header, Footer],
 	templateUrl: './dashboard.html',
 	styleUrl: './dashboard.scss',
 })
